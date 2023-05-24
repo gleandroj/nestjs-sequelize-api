c1895506613f78e3bb0d98dcad49ae7c53f9ea9c
Gabriel Siqueira <gabrielleandrojunior@live.com>
2023-05-24 09:34:07 -0300

    feature: added psql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: DB_HOST,
+      port: DB_PORT ? parseInt(DB_PORT) : 5432,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
